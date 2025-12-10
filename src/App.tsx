84bf45ce6a57c5730916a114c0da5ce97be125cc
Stan <Stanwinbenny2k07@gmail.com>
2025-12-10 13:58:35 +0530

    feat: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ekom</h1>
+      </header>
+
+      <main>
+        <p>BLEGH</p>
+      </main>
+      <footer>
+        <p>&copy;HUGE COMPANY</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
